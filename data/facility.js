0ca4c394bf4d383f7bdbdd79fd7a34d6f62f7844
Thomas Dy <thatsmydoing@gmail.com>
2016-06-05 23:28:23 +0800

    Parse facilities

---
+++ data/facility.js
@@ -0,0 +1,87 @@
+var _ = require('lodash');
+var util = require('./util');
+var struct = require('./struct');
+
+function parse(alphax, blurbsx, helpx) {
+  var lines = util.getList('FACILITIES', alphax);
+  var facLines = lines.slice(0, 69);
+  var projLines = lines.slice(69);
+
+  return {
+    base: parseType(
+      facLines,
+      parseFacility,
+      'FAC',
+      id => id > 33 ? id + 31 : id,
+      'HELPFAC',
+      blurbsx,
+      helpx
+    ),
+    project: parseType(
+      projLines,
+      parseSecretProject,
+      'PROJECT',
+      _.identity,
+      'HELPPROJ',
+      blurbsx,
+      helpx
+    )
+  }
+}
+
+function parseType(lines, parseItem, blurbHeader, idMap, helpHeader, blurbsx, helpx) {
+  var list = lines
+    .map(util.splitComma)
+    .map(parseItem)
+    .filter(validBuilding)
+
+  var byIndex = struct.buildIndex(list, 'index');
+
+  addBlurbs(byIndex.find, blurbHeader, idMap, blurbsx);
+  addHelp(byIndex.find, helpHeader, helpx);
+  return list;
+}
+
+function parseFacility(a, index) {
+  return {
+    index: index+1,
+    name: a[0],
+    cost: parseInt(a[1]),
+    maintenance: parseInt(a[2]),
+    prerequisite: a[3],
+    // free is unused
+    effect: a.slice(5).join(', ')
+  }
+}
+
+function parseSecretProject(a, index) {
+  var result = parseFacility(a, index);
+  result.index = index;
+  result.aggressiveness = parseInt(a[6]);
+  result.weights = {
+    conquer: parseInt(a[7]),
+    discover: parseInt(a[8]),
+    build: parseInt(a[9]),
+    explore: parseInt(a[10])
+  }
+  return result;
+}
+
+function validBuilding(item) {
+  return item.prerequisite != 'Disable';
+}
+
+function addBlurbs(byIndex, header, idMap, blurbsx) {
+  util.getEntries(header, blurbsx).forEach(item => {
+    var id = idMap(item.index);
+    byIndex(id).blurb = item.entry;
+  });
+}
+
+function addHelp(byIndex, header, helpx) {
+  util.getEntries(header, helpx).forEach(item => {
+    byIndex(item.index).text = item.entry;
+  });
+}
+
+module.exports = { parse };
--- data/parse.js
+++ data/parse.js
@@ -3,6 +3,7 @@ var _ = require('lodash');
 var util = require('./util');
 var tech = require('./tech');
 var concept = require('./concept');
+var facility = require('./facility');
 
 function readData(name) {
   return fs.readFileSync(name+'.txt', { encoding: 'ASCII' }).replace(/\r/g, '');
@@ -11,13 +12,17 @@ function readData(name) {
 var alphax = readData('alphax');
 var blurbsx = readData('blurbsx');
 var conceptsx = readData('conceptsx');
+var helpx = readData('helpx');
 
 var techs = tech.parse(alphax, blurbsx);
 var concepts = concept.parse(conceptsx);
+var facilities = facility.parse(alphax, blurbsx, helpx);
 
 var output = 'module.exports = ' + JSON.stringify({
   techs: techs,
   concepts: concepts.basic,
   advConcepts: concepts.advanced,
+  baseFacilities: facilities.base,
+  secretProjects: facilities.project
 });
 fs.writeFileSync('../src/data.js', output);
