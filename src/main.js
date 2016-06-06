0713044ed3c85dbba4cfb956f3976efd31073c67
Thomas Dy <thatsmydoing@gmail.com>
2016-06-06 18:46:00 +0800

    Build routes and redirect from data

--- src/main.js
+++ src/main.js
@@ -6,42 +6,50 @@ import ConceptView from './ConceptView.vue'
 import FacilityView from './FacilityView.vue'
 import TerraformView from './TerraformView.vue'
 
+import {chain, map} from 'lodash'
+import {dictionary} from './lookup'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter();
-router.map({
-  '/tech/:id': {
-    name: 'tech',
-    component: TechView
-  },
-  '/concept/:id': {
-    name: 'concept',
-    component: ConceptView
-  },
-  '/adv-concept/:id': {
-    name: 'adv-concept',
-    component: ConceptView
-  },
-  '/facility/:id': {
-    name: 'facility',
-    component: FacilityView
-  },
-  '/project/:id': {
-    name: 'project',
-    component: FacilityView
-  },
-  '/terraform/:id': {
-    name: 'terraform',
-    component: TerraformView
-  }
-})
-router.redirect({
-  '/concept/': '/concept/altitude',
-  '/adv-concept/': '/adv-concept/altitude-advanced',
-  '/tech/': '/tech/adaptive-doctrine',
-  '/facility/': '/facility/aerospace-complex',
-  '/project/': '/project/ascent-to-transcendence',
-  '/terraform/': '/terraform/build-mag-tube',
-  '*': '/concept/'
-})
+const viewMapping = {
+  tech: TechView,
+  concept: ConceptView,
+  'adv-concept': ConceptView,
+  facility: FacilityView,
+  project: FacilityView,
+  terraform: TerraformView
+};
+const routeMap = chain(dictionary)
+  .map(value => {
+    const category = value.slug;
+    return [
+      '/'+category+'/:id',
+      {
+        name: category,
+        component: viewMapping[category]
+      }
+    ]
+  })
+  .fromPairs()
+  .value();
+
+const redirect = chain(dictionary)
+  .map(value => {
+    const category = value.slug;
+    let slugs = map(value.list, 'slug');
+    slugs.sort();
+    return [
+      '/'+category+'/',
+      '/'+category+'/'+slugs[0]
+    ]
+  })
+  .fromPairs()
+  .assign({
+    '*': '/concept/'
+  })
+  .value();
+
+router.map(routeMap);
+router.redirect(redirect)
 router.start(App, 'app');
