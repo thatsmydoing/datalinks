c17fca4488a55e2ee04dc5c5be9159d10dc32ece
Thomas Dy <thatsmydoing@gmail.com>
2016-06-11 13:46:19 +0800

    Add weapon images

--- src/WeaponView.vue
+++ src/WeaponView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="weapon-view info-panel">
-    <span>Firepower: {{ item.power }}</span>
-    <span v-if="showCost">Cost: {{ item.cost }}</span>
-    <span>Prerequisite: <item-link :item="item.prerequisite"></item-link></span>
+    <div class="general" v-bind:class="{'has-image': image}">
+      <img v-if="image" :src="image" />
+      <div class="info">
+        <span>Firepower: {{ item.power }}</span>
+        <span v-if="showCost">Cost: {{ item.cost }}</span>
+        <span>Prerequisite: <item-link :item="item.prerequisite"></item-link></span>
+      </div>
+    </div>
     <markup-view :text="item.text"></markup-view>
   </div>
 </template>
@@ -13,6 +18,10 @@ import ViewMixin from './ViewMixin'
 export default {
   mixins: [ ViewMixin ],
   computed: {
+    image() {
+      let name = this.item.id.toLowerCase().replace(/ /g, '-');
+      return require('../img/weapons/'+name+'.png');
+    },
     showCost() {
       return this.item.power != this.item.cost;
     }
@@ -22,6 +31,25 @@ export default {
 
 <style scoped>
 .weapon-view {
+  display: flex;
+  flex-direction: column;
+}
+
+.general {
+  display: flex;
+}
+
+.general.has-image {
+  margin: 10px;
+}
+
+.general img {
+  margin-right: 15px;
+  height: 200px;
+  width: 260px;
+}
+
+.info {
   display: flex;
   flex-direction: column;
 }
